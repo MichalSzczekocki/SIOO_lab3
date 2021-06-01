1956aef3c6afd6bbdaeaf67bafb0bfa6fa24baa3
MichalSzczekocki <michal.szczekocki.poznan@gmail.com>
2021-06-01 12:25:22 +0200

    test

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
-from sys import stdin
-import itertools
-EPS = 1e-4
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QRadioButton, QComboBox, QSlider, \
+    QPushButton
+from PyQt5.QtCore import Qt
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Position:
-    def __init__(self, column, row):
-        self.column = column
-        self.row = row
+class Polynomial:
 
-# Takes an int "n" that defines how many total inequalty equations there are
-# Takes an int "m" that defines how many total variables there are
-# Takes a list of lists "a" of size n x m that contains the inequalities.
-# Takes a list "b" of size n of the maximums of each inequality
-# Takes a list "c" that represents the optimization function to maximize
-# For example:
-# x + y - 3z <= 10
-# -5x + 10y <= 50
-# 3x - 2y -4z <= 9
-# Maximize: x + 6y -3z
-# n, m = 3, 3
-# a = [[1,1,-3],[-5,10,0],[3,-2,-4]]
-# b = [10, 50, 9]
-# c = [1, 6, -3]
-def ReadEquation():
-    n, m = map(int, input().split())
-    a = []
-    for row in range(n):
-        a.append(list(map(float, input().split())))
-    b = list(map(float, input().split()))
-    c = list(map(float, input().split()))
-    return a, b, c, n, m
+    def __init__(self, *coefficients):
+
+        self.coefficients = list(coefficients)  # tuple is turned into a list
+
+    def __repr__(self):
+
+        return "Polynomial" + str(tuple(self.coefficients))
+
+    def __str__(self):
+
+        def x_expr(degree):
+            if degree == 0:
+                res = ""
+            elif degree == 1:
+                res = "x_1"
+            else:
+                res = "x_" + str(degree)
+            return res
+
+        degree = len(self.coefficients) - 1
+        res = ""
+
+        for i in range(0, degree + 1):
+            coeff = self.coefficients[i]
+            # nothing has to be done if coeff is 0:
+            if abs(coeff) == 1 and i < degree:
+                # 1 in front of x shouldn't occur, e.g. x instead of 1x
+                # but we need the plus or minus sign:
+                res += f"{'+' if coeff > 0 else '-'}{x_expr(degree - i)}"
+            elif coeff != 0:
+                res += f"{coeff:+g}{x_expr(degree - i)}"
+
+        return res.lstrip('+')  # removing leading '+'
+
+    def __call__(self, x):
+        res = 0
+        for coeff in self.coefficients:
+            res = res * x + coeff
+        return res
 
 if __name__ == "__main__":
-
+    A = Polynomial(6,4,5,0)
+    print(A)
     exit(0)
